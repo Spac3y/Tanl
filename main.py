7fa8e7289bf1d04ddff71dda703aa471b15dd94c
spac3y <73653851+Spac3y@users.noreply.github.com>
2025-01-22 21:04:24 +0200

    Event logging

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import gspread
+import logging
+from logging.handlers import RotatingFileHandler
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
@@ -12,8 +14,23 @@ headers = {
 	"Authorization" : None
 }
 
+# Configure the RotatingFileHandler
+log_handler = RotatingFileHandler(
+    "logfile.log",  # Log file name
+    maxBytes=1_000_000,  # Maximum file size in bytes (1 MB in this example)
+    backupCount=0  # No backup files; overwrite the same file
+)
+
+logging.basicConfig(
+    level=logging.INFO,  # Set the logging level
+    format="%(asctime)s - %(levelname)s - %(message)s",  # Log message format
+    handlers=[log_handler]  # Add the rotating handler
+)
+
 with open("whatsappKey.txt", 'r') as f:
 	headers['Authorization'] = "Bearer " + f.read()
+
+logging.info(f"Succesfully read WhatsappKey.txt -> {headers['Authorization']}")
 
 dataJson = {
   "messaging_product": "whatsapp",
@@ -41,6 +58,8 @@ dataJson = {
 
 creds = ServiceAccountCredentials.from_json_keyfile_name("key.json", scope)
 client = gspread.authorize(creds)
+logging.info(f"Succesfully read key.json")
+
 
 with open("remember.json", 'r') as f:
 	temp = json.loads(f.read())
@@ -73,6 +92,9 @@ for i in range(len(nameCol)): # Go through every user and send them a custom mes
 	dataJson['template']['components'][0]['parameters'][0]['text'] = nameCol[i][0]
 	print(f"{dataJson['to']} -> {dataJson['template']['components'][0]['parameters'][0]['text']}") # Actual live data taken from sheets
 	response = requests.request("POST", url, data=json.dumps(dataJson), headers=headers)
-
+	if(response.status_code == 200):
+		logging.info(f"[{response.status_code}] Sent {nameCol[i][0]} : 40{phNrCol[i][0]} template message named - {dataJson['template']['name']}")
+	else:
+		logging.error(f"[{response.status_code}] {response.text}")
 	# print(response.text)
 	print(response.status_code)
