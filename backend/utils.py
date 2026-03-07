58d95e1a4d247d54148d7d8d4ac1593279c5de51
spac3y <73653851+Spac3y@users.noreply.github.com>
2026-03-07 12:48:02 +0200

    remove all old files

--- backend/utils.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-
-def getCurrentTime():
-	now = datetime.now().strftime("%y-%m-%d %H:%M:%S")
-	return now
--- frontend/__init__.py
+++
@@ -1,8 +0,0 @@
-from errorhandlers import
-from google_login import 
-from index import 
-from login import 
-from profile_updates import
-from profile import 
-from status import 
-from user_info import
